5c423d2f70d7ff13907d6f63e06ce6bcac30afab
Justin Albert <Jnalbert879@gmail.com>
2021-06-11 18:18:40 -0700

    added Layout of every page

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app';
+import Layout from "../comps/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    
+  )
+  
 }
 
 export default MyApp
